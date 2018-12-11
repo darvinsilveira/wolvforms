2499006e4edd0344b5c77576d1fd903b77eaafa8
Gilberto Tavares <camaleaun@gmail.com>
2018-12-11 11:17:48 -0200

    Partial modified generate scaffold

---
+++ wolvforms.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WolvForms
+ * Description: TODO
+ * Author: Envolve Labs
+ * Author URI: http://envolvelabs.com.br
+ * Text Domain: wolvforms
+ * Domain Path: /languages
+ * Version: 1.0.0
+ *
+ * @package WolvForms
+ */
+
+defined( 'ABSPATH' ) || exit;
